6c2711e680444766a98e034b5d17045440dbdf71
Justin Hertik <jhertik1@cs.binghamton.edu>
2021-12-01 21:32:09 -0500

    Started some implementation of Graph, will refine when graph generator is completed

--- Graph.java
+++ Graph.java
@@ -1,16 +1,50 @@
 //Graph.java
+import java.util.List;
 
 public abstract class Graph implements GraphInterface {
 
+	private List<Node> nodeList;
+	private List<Edge> edgeList;
+	private Integer nodes;
+
 	public Graph() {
-		//init();
+		nodes = 0;
+		init();
 	}
 
-	public void init() {}
+	public void init() {
+		nodeList = new List<Node>();
+		edgeList = new List<Node>();
+	}
 
-	public void AddNode(/*Node n*/) {}
+	//To add nodes, just add to a list
+	public void AddNode(Node n) {
+		nodeList.add(n);
+	}
 
-	public void generateMinSpanningTree() {}
+	/*
+	To add an edge to the graph, just specify the two nodes and connect them
+	with specified weight
+	*/
+	public void addEdge(Node src, Node dest, Integer weight) {
+
+		src.addEdge(dest, weight, false); //Adds both edges (u,v) and (v,u)
+
+	}
+
+	public void generateMinSpanningTree() {
+	
+		//Implement Kruskals
+	
+	}
+
+	public void DFS() { //Call DFS to add all possible edges to the list, and prevent repeats from appearing which would hinder the heaps
+
+		for(Node n : nodeList) {
+
+		}
+
+	}
 
 	public abstract void getShortestPath();
 	
